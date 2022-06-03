14851d24b08c7237a150d90025ed08a5f4ded4df
Iryna Vakilova <iravakilova@gmail.com>
2022-06-03 19:53:54 +0300

    task-2 done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ulIngredients = document.querySelector("#ingredients");
+const item = ingredients
+  .map((ingredient) => `<li>${ingredient}</li>`)
+  .join("");
+console.log(item);
+ulIngredients.innerHTML = item;
+
+  const newIngredient = document.createElement("li");
+  newIngredient.textContent = "Інгредієнт";
+  newIngredient.classList.add("item");
+ulIngredients.appendChild(newIngredient);
+  
+
