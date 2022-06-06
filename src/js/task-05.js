9d182c58ba65e2c1d835b5c4d8e01d306abdbcc0
Iryna Vakilova <iravakilova@gmail.com>
2022-06-06 18:47:08 +0300

    task-5 done

---
+++ src/js/task-05.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
+
+input.addEventListener("input", onInputChange);
+function onInputChange(event) {
+    output.textContent =
+        event.currentTarget.value === ""
+            ? "Anonymous"
+            : event.currentTarget.value;
+}
