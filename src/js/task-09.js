973f66b9572ec96895d2035d7ffbee05f76ef6f8
Iryna Vakilova <iravakilova@gmail.com>
2022-06-08 19:27:06 +0300

    task-9 done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const body = document.querySelector('body');
+const changeColor = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+
+changeColor.addEventListener("click", onChange)
+function onChange() {
+  color.textContent = body.style.backgroundColor;
+  body.style.backgroundColor = getRandomHexColor();
+  console.log(getRandomHexColor);
 }
