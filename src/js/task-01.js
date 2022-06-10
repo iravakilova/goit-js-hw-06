50083b9482a64c15b4db1dc9d5c654dfbe46b81b
Iryna Vakilova <iravakilova@gmail.com>
2022-06-10 21:13:58 +0300

    update task-1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,9 +1,11 @@
-const ulCategories = document.querySelectorAll(".item");
-console.log(`Number of categories: ${ulCategories.length}`);
-for (const item of ulCategories) {
-    const categoryTitle = document.querySelector("h2");
-    console.log(`Category: ${categoryTitle.textContent}`);
-    const categoryList = item.querySelectorAll("li");
-    console.log(`Elements: ${categoryList.length}`);
-};
+const ulCategories = document.querySelector('#categories');
 
+const liItem = document.querySelectorAll('.item');
+console.log(`Number of categories: ${liItem.length}`);
+
+[...ulCategories.children].forEach((item) => {
+    const categoryTitle = item.querySelector('h2');
+    console.log(`Category: ${categoryTitle.textContent}`);
+    const categoryList = item.querySelectorAll('li');
+    console.log(`Elements: ${categoryList.length}`);
+});
