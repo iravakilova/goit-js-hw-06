50f6c963917f5bc9626b7bfebb6834820194ca3a
Iryna Vakilova <iravakilova@gmail.com>
2022-06-11 16:09:20 +0300

    update task-4

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -2,10 +2,14 @@ const decrButton = document.querySelector('[data-action="decrement"]');
 const incrButton = document.querySelector('[data-action="increment"]');
 const value = document.querySelector('#value');
 let counterValue = 0;
+function newValue(number) {
+    value.textContent = number;
+}
 decrButton.addEventListener("click", () => {
-    counterValue = value.textContent--;
+    counterValue--;
+    newValue(counterValue);
 });
 incrButton.addEventListener("click", () => {
-    counterValue = value.textContent++;
+    counterValue++;
+    newValue(counterValue);
 });
-
